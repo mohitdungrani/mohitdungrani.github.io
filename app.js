5bb9f66feb3e7801b1a2c69b1b8907963e6035d3
Mohit Dungrani <48024401+mohitdungrani@users.noreply.github.com>
2021-02-17 22:59:16 +0530

    Update app.js
    Post name Changed

--- app.js
+++ app.js
@@ -30,35 +30,39 @@ $(document).ready(() => {
     });
     function text_chage(){
         var $text = $('.my_post');
-        var data = ['Freelancer', 'Designer', 'Developer'];
-        text_sequence(0);
-        function text_sequence(i){
-            // $text.html("");
-            if(data.length > i){
-                setTimeout(function(){
-                    // $text.html(data[i]);
-                    $.each(data[i].split(''), function(j, letter){
-                        setTimeout(function(){
-                            $text.html($text.html() + letter)
-                        }, 100*j);
-                    });
-                    l = 0;
-                    len = data[i].length;
-                    for(k=0; k < data[i].length ; k++){
-                        setTimeout(function(){
-                            len = len - 1;
-                            $text.html(data[i].substring(0, len))
-                        }, k*100 + 1000 + ( data[i].length * 200 ));
-                    }
+        var data = ['Data Scientist','ML Engineer','Freelancer'];
+        
+        function addTextCharacter(i){
+            return new Promise(function(resolve, reject){
+                $.each(data[i].split(''), function(j, letter){
                     setTimeout(function(){
-                        text_sequence(++i);
-                    }, 3800);
-                }, 1000);
-            }
-            else{
-                text_sequence(0);
-            }
+                        $text.html($text.html() + letter)
+                    }, 50*j);
+                });
+                setTimeout(resolve,data[i].length*50 + 2000);
+            });
+            
         }
+        function removeTextCharacter(i){
+            return new Promise(function(resolve, reject){
+                len = data[i].length;
+                for(k=0; k < data[i].length ; k++){
+                    setTimeout(function(){
+                        len = len - 1;
+                        $text.html(data[i].substring(0, len))
+                    }, k*50);
+                }
+                setTimeout(resolve,data[i].length*50 + 1000)
+            });
+            
+        }
+        async function text_sequence(i){
+            await addTextCharacter(i);
+            await removeTextCharacter(i);
+            i = i + 1;
+            text_sequence(i < data.length ? i :0);
+        }
+        text_sequence(0);    
     }
     text_chage();
 });
